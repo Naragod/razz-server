56fa33333420d4aa10f6df4b30dad3a9e3611ba7
Nara <Naragod>
2019-07-14 16:01:55 -0400

    Updated package references, and changed the name of a file.

---
+++ db/dbHandler.js
@@ -0,0 +1,44 @@
+const { Client } = require('pg');
+ 
+
+promify = function(callback) {
+    return new Promise((resolve, reject) => {
+        callback(resolve, reject);
+    });
+};
+
+module.exports = {
+    setClientConnection: (config) => {
+        let client = new Client(config);
+        client.connect();
+        return client;
+    },
+ 
+    query: (client, query) => {
+        return promify((resolve, reject) => {
+            client.query(query, (err, res) => {
+                if(err){
+                    console.log("Could not perform query:", err);
+                    reject(err);
+                }
+                resolve(res);
+            });
+        });
+    },
+
+    insert: (client, into, values) => {
+        return promify((resolve, reject) => {
+            client.query(into, values, (err, res) => {
+                if(err){
+                    console.log("Cound not insert:", err);
+                    reject(err);
+                }
+                resolve(res);
+            });
+        });
+    },
+ 
+    endConnection: (connection) => {
+        connection.end();
+    }
+}
